5c9e773126c7890e1e121c85d9d4bdc726fa3ff1
bmoraleTSU <bmorale1@my.tnstate.edu>
2022-11-06 18:09:21 -0600

    Removed footer so that client list can fill whole screen.

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import "bulma/css/bulma.min.css";
 import "./index.css";
 import App from "./App";
 import Header from "./Views/Header";
-import Footer from "./Views/Footer";
+// import Footer from "./Views/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     
     <Header/>
-    <div className="section is-small is-fluid has-background-light">
+    <div className="section is-large is-fluid has-background-light">
       <App />
     </div>
-    <Footer/>
+    {/* <Footer/> */}
   </React.StrictMode>
 );
 
